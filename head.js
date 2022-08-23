9c7f4e0b5023535618880a63164d4bd1cf6e7da4
Jordan Gertenstein <jordangert@hotmail.com>
2022-08-23 12:40:56 -0500

    added semicolons

--- head.js
+++ head.js
@@ -11,7 +11,7 @@ assertEqual(1, 1);
 
 const head = function(array) {
   return array[0];
-}
+};
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello")
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
