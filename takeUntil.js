8b3b55b2b6917c53b19f3007f26019034e970f62
Jordan Gertenstein <jordangert@hotmail.com>
2022-08-28 16:59:00 -0500

    refactored with Gary

---
+++ takeUntil.js
@@ -0,0 +1,17 @@
+const takeUntil = function(array, callback) {
+  const results = [];
+
+  for (const item of array) {
+    const returnValue = callback(item);
+
+    if (!returnValue) {
+      results.push(item);
+    }
+  }
+  return results;
+
+};
+
+
+let result = takeUntil([1, 2, 3, 4, 5], x => x > 2);
+console.log(result);
