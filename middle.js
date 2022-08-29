2eba52b624917fedae1ba2c5c4526ed53fb7058f
Jordan Gertenstein <jordangert@hotmail.com>
2022-08-29 15:07:04 -0500

    edited

--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(firstArray, secondArray) {
-  if (eqArrays(firstArray, secondArray)) {
-    return console.log(`✅✅✅ Assertion Passed: ${firstArray} === ${secondArray}`);
-  }
-
-  console.log(`🛑🛑🛑 Assertion Failed: ${firstArray} !== ${secondArray}`);
-};
-
 const middle = function(arr) {
   let finale = [];
   if (arr.length < 3) {
