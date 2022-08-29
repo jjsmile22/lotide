f0bced2bd595e7520675c8dccc12569f3bf8b4b0
Jordan Gertenstein <jordangert@hotmail.com>
2022-08-29 07:43:10 -0500

    refactored, exported tailed.js

--- tail.js
+++ tail.js
@@ -1,29 +1,13 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  }
+const assertEqual = require('./assertEqual');
 
-  console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-};
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
-const tail = function(arrayOne) {
-  let arrNew = [];
-  for (let i = 1; i < arrayOne.length; i++) {
-    arrNew.push(arrayOne[i]);
-  }
-  console.log(arrNew);
-};
-
-const tail2 = function(arrayThree) {
+const tail = function(arrayThree) {
   let final = arrayThree.slice(1);
   console.log(final);
   return final;
 };
 
-tail([1, 2, 3, 4]);
-tail2([1, 2, 3, 4]);
+module.exports = tail;
 
-assertEqual(tail, tail2);
+
+
+
