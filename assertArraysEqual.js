91fc7c7f5bc738086999b00b75803ef602e23b3d
Jordan Gertenstein <jordangert@hotmail.com>
2022-08-29 07:57:52 -0500

    refactored, exported, removed test

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,4 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(firstArray, secondArray) {
   if (eqArrays(firstArray, secondArray)) {
@@ -18,7 +8,4 @@ const assertArraysEqual = function(firstArray, secondArray) {
   console.log(`🛑🛑🛑 Assertion Failed: ${firstArray} !== ${secondArray}`);
 };
 
-assertArraysEqual(["one", 'two'], ["one", 'two']);
-
-assertArraysEqual([12, 34], [12, 34]);
-assertArraysEqual([12, 4], [12, 34]);
+module.exports = assertArraysEqual;
