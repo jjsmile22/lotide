730bf395dd11714d5ffd08060bb9b090d17ce75a
Jordan Gertenstein <jordangert@hotmail.com>
2022-08-29 15:03:27 -0500

    modified test to use chai assertions

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,4 +1,18 @@
+const assert = require('chai').assert;
 const assertEqual = require('../assertEqual');
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+describe("#assertEqual", () => {
+  it("returns false", () => {
+    assert.strictEqual(assertEqual(['one', 'two'], ['one', 'two']));
+  });
+
+  it("returns 1 for 1", () => {
+    assert.strictEqual(assertEqual(1, 1)); 
+  });
+
+});
+
+
+
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
