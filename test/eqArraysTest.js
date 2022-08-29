8e3517e15a7d3816a91f660820aef8f7af4716bb
Jordan Gertenstein <jordangert@hotmail.com>
2022-08-29 07:50:29 -0500

    adding eqArrayTest.js to repo

---
+++ test/eqArraysTest.js
@@ -0,0 +1,13 @@
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+console.log(eqArrays([1, 2, 3], [3, 2, 1]));
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
+
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
+
+
+
